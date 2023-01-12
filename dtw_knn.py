734faba0f387af14e44a360b998dea8f35c4a56d
colorful <86513000+wefwefWEF2@users.noreply.github.com>
2023-01-12 18:08:58 +0800

    Update dtw_knn.py

--- dtw_knn.py
+++ dtw_knn.py
@@ -1,3 +1,9 @@
+import numpy as np
+from scipy.spatial import distance
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 import os
 import pandas as pd
 import sklearn
@@ -7,10 +13,55 @@ from scipy.spatial import distance
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
+#
+#toy dataset
+# X = np.random.random((100,10))
+# print(X.shape)
+# y = np.random.randint(0,2, (100))
+# print(y.shape)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+#
+# print(type(X_train))
+# print(y_train)
+# print(X_test.shape)
+
+
+
+
+data_path= r'/code/DTW/data_ceshiji_train'
+
+# data_path= r'/code/tsc/dataset/data/data_train'
+
+
+y_train = pd.read_csv(os.path.join(data_path, 'train_labels.csv'))
+y_test = pd.read_csv(os.path.join(data_path, 'test_labels.csv'))
+
+X_train = pd.read_csv(os.path.join(data_path, 'train_new.csv'))
+X_test = pd.read_csv(os.path.join(data_path, 'test_new.csv'))
+
+nb_classes = len(np.unique(np.concatenate((y_train, y_test), axis=0)))
+
+# transform the labels from integers to one hot vectors
+enc = sklearn.preprocessing.OneHotEncoder(categories='auto')
+enc.fit(np.concatenate((y_train, y_test), axis=0).reshape(-1, 1))
+y_train = enc.transform(y_train.values.reshape(-1, 1)).toarray()
+y_test = enc.transform(y_test.values.reshape(-1, 1)).toarray()
+
+# save orignal y because later we will use binary(lablel转化为2进制)
+y_true = np.argmax(y_test, axis=1)
+
+if len(X_train.shape) == 2:  # if univariate
+    # add a dimension to make it multivariate with one dimension
+    x_train = X_train.values.reshape((X_train.shape[0], X_train.shape[1]))
+    x_test = X_test.values.reshape((X_test.shape[0], X_test.shape[1]))
+
+print(type(X_train))
+print(X_train.shape)
+print(y_train)
 
 
 #custom metric
-def DTW(a, b):
+def DTW(a, b):   
     an = a.size
     bn = b.size
     pointwise_distance = distance.cdist(a.reshape(-1,1),b.reshape(-1,1))
@@ -24,55 +75,22 @@ def DTW(a, b):
                                    cumdist[ai, bi]])
             cumdist[ai+1, bi+1] = pointwise_distance[ai,bi] + minimum_cost
 
-    return cumdist[an, bn]
+    # print(cumdist[an, bn])
+    return cumdist[an, bn]#累积概率？
 
 
 
 
 
-
-def create_classifier(data_path):
-    # x_train, x_test, y_train, y_test = train_test_split(train, train_labels)
-
-    y_train = pd.read_csv(os.path.join(data_path, 'train_labels.csv'))
-    y_test = pd.read_csv(os.path.join(data_path, 'test_labels.csv'))
-
-    x_train = pd.read_csv(os.path.join(data_path, 'train_new.csv'))
-    x_test = pd.read_csv(os.path.join(data_path, 'test_new.csv'))
-
-    nb_classes = len(np.unique(np.concatenate((y_train, y_test), axis=0)))
-
-    # transform the labels from integers to one hot vectors
-    enc = sklearn.preprocessing.OneHotEncoder(categories='auto')
-    enc.fit(np.concatenate((y_train, y_test), axis=0).reshape(-1, 1))
-    y_train = enc.transform(y_train.values.reshape(-1, 1)).toarray()
-    y_test = enc.transform(y_test.values.reshape(-1, 1)).toarray()
-
-    # save orignal y because later we will use binary(lablel转化为2进制)
-    y_true = np.argmax(y_test, axis=1)
-
-    if len(x_train.shape) == 2:  # if univariate
-        # add a dimension to make it multivariate with one dimension
-        x_train = x_train.values.reshape((x_train.shape[0], x_train.shape[1]))
-        x_test = x_test.values.reshape((x_test.shape[0], x_test.shape[1]))
-
-
-    print(x_train.shape)
-
-
-    # train
-    parameters = {'n_neighbors': [2, 4, 8]}
-    clf = GridSearchCV(KNeighborsClassifier(metric=DTW), parameters, cv=3, verbose=1)
-    clf.fit(x_train, y_train)
-
-    # evaluate
-    y_pred = clf.predict(x_test)
-    print(classification_report(y_test, y_pred))
+#train
+#n_neighbors : 表示选择距离最近的K个点来投票的数量。
+parameters = {'n_neighbors':[1]}
+clf = GridSearchCV(KNeighborsClassifier(metric=DTW), parameters, cv=3, verbose=1)
+clf.fit(X_train, y_train)
 
 
 
-if __name__ == '__main__':
-    data_path= r'/code/tsc/dataset/11lei_ori_video_experiment/data_train'
-    create_classifier(data_path)
-
-
+#evaluate
+y_pred = clf.predict(X_test)
+print(y_pred)
+print(classification_report(y_test, y_pred))
